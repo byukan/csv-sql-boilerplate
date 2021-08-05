dc3d235b73037d3039aaf8057bd7faa04a18d8b2
Brant Yukan <brant.yukan@gmail.com>
2021-08-04 23:20:14 -0700

    unused pandas implementation

--- scratch/pandas_most_active_cookie.py
+++ scratch/pandas_most_active_cookie.py
@@ -5,12 +5,6 @@ from pandasql import sqldf
 
 
 def main():
-    # parser = argparse.ArgumentParser(description='Process some integers.')
-    # parser.add_argument('--sum', dest='accumulate', action='store_const',
-    # const = sum, default = max,
-    # help = 'sum the integers (default: find the max)')
-    # print([*parser.parse_args()])
-
     day = "'2018-12-08'"
     cookie_log = pd.read_csv('cookie_log.csv')
     most_active_df = sqldf(f"""
